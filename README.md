53b083d8a8f916d3b2a5b66f030283b38b61445b
kanga333 <e411z7t40w@gmail.com>
2017-12-01 06:38:59 +0900

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # header-echo
 header-echo is a server that echoes the request header
+
+I referred it.
+https://qiita.com/minamijoyo/items/5247dbb82f7fb2847d87
---
+++ files/echo_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+class RequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write('%s %s %s\n' % (self.command, self.path, self.request_version))
+        self.wfile.write(self.headers)
+        self.finish()
+        self.connection.close()
+
+port = 80
+server = HTTPServer(('', port), RequestHandler)
+server.serve_forever()
